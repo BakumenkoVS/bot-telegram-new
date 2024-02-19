f034c1520d040952217d1162f4553b2842d1d064
vlad bakumenko <tinyyy11@yandex.ru>
2024-02-19 14:04:23 +0300

    kjfn

--- bot/connections/db.connection.js
+++ bot/connections/db.connection.js
@@ -1,16 +1,11 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize("postgres://postgres:postgres@localhost/bot", {
-   host: process.env.POSTGRES_HOST,
-   port: process.env.POSTGRES_PORT,
-   dialect: process.env.POSTGRES_DIALECT,
-});
 
 module.exports = new Sequelize(
    `${process.env.POSTGRES_DATABASE}`,
    `${process.env.POSTGRES_USERNAME}`,
-   `${process.env.postgres.POSTGRES_PASSWORD}`,
+   `${process.env.POSTGRES_PASSWORD}`,
    {
       host: process.env.POSTGRES_HOST,
       port: process.env.POSTGRES_PORT,
