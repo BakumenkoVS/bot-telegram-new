be2259ca57bc705f0690a45d4c9292f1569a1940
vlad bakumenko <tinyyy11@yandex.ru>
2024-02-19 13:52:55 +0300

    proba

--- bot/connections/db.connection.js
+++ bot/connections/db.connection.js
@@ -1,13 +1,8 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize(
-   "postgres",
-   process.env.POSTGRES_USERNAME,
-   process.env.POSTGRES_PASSWORD,
-   {
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      dialect: process.env.POSTGRES_DIALECT,
-   }
-);
+module.exports = new Sequelize("postgres://postgres:postgres@localhost/bot", {
+   host: process.env.POSTGRES_HOST,
+   port: process.env.POSTGRES_PORT,
+   dialect: process.env.POSTGRES_DIALECT,
+});
