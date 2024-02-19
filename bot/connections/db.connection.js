b9c46cd63de6ac0ff5c1fc5f552bccde8c5e1839
vlad bakumenko <tinyyy11@yandex.ru>
2024-02-19 14:03:11 +0300

    hop

--- bot/connections/db.connection.js
+++ bot/connections/db.connection.js
@@ -6,3 +6,14 @@ module.exports = new Sequelize("postgres://postgres:postgres@localhost/bot", {
    port: process.env.POSTGRES_PORT,
    dialect: process.env.POSTGRES_DIALECT,
 });
+
+module.exports = new Sequelize(
+   `${process.env.POSTGRES_DATABASE}`,
+   `${process.env.POSTGRES_USERNAME}`,
+   `${process.env.postgres.POSTGRES_PASSWORD}`,
+   {
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      dialect: process.env.POSTGRES_DIALECT,
+   }
+);
