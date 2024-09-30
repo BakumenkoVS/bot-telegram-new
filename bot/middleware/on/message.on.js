b40a6477ddec88ac8e505a2bccc2b5132b22c3eb
vlad bakumenko <tinyyy11@yandex.ru>
2024-09-30 16:52:28 +0300

    fix path

--- bot/middleware/on/message.on.js
+++ bot/middleware/on/message.on.js
@@ -13,7 +13,7 @@ module.exports = bot.on("text", async (ctx) => {
 
          await ctx.replyWithPhoto(
             {
-               source: path.resolve(__dirname, "../../img/moreinfo.jpg"),
+               source:  "/root/bot-telegram-new/img/moreInfo.jpg",
             },
             {
                caption:
