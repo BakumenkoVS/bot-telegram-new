7294e0bbda56357f622acf1c489ac9ae149e6d2a
vlad bakumenko <tinyyy11@yandex.ru>
2025-04-10 11:11:02 +0300

    Обновление цен в константах: изменены значения низкой и средней цен для повышения актуальности информации.

--- bot/utils/bot-helpers.js
+++ bot/utils/bot-helpers.js
@@ -8,8 +8,8 @@ const CONSTANTS = {
   IMG_DIR: "img",
   CURRENCY: "RUB",
   PRICES: {
-    LOW_PRICE: 5490 * 100, // в копейках
-    MIDDLE_PRICE: 6490 * 100, // в копейках
+    LOW_PRICE: 5999 * 100, // в копейках
+    MIDDLE_PRICE: 6999 * 100, // в копейках
     HIGH_PRICE: 15990 * 100, // в копейках
     TEST_PRICE: 65 * 100, // в копейках
   },
